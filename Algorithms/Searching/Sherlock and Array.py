cffb682052ffa7736fc35fabb9e792f83ef81263
Acc Team <kshtjtcs@outlook.com>
2019-02-11 17:57:20 +0530

    Sherlock and array solution corrected

--- Algorithms/Searching/Sherlock and Array.py
+++ Algorithms/Searching/Sherlock and Array.py
@@ -1,27 +1,21 @@
 def balancedSum(n,arr):
-    arr_diff = arr[0]
-    for i in range(1,n):
-        arr_diff -= arr[i]    
+    rSum = 0
+    lSum = 0
+    for item in arr:
+        rSum += item
     
-    if(binary_search(arr,abs(arr_diff),0,n-1) == None):        
-        return 'NO'
-    else:
-        return 'YES'
+    for i in range(0,n):
+        rSum -= arr[i]
+        if(rSum == lSum):
+            return 'YES'
+        lSum += arr[i]
+    return 'NO'
     
-def binary_search(arr,key,L,R):
-    if R >= L:
-        mid = int(L + (R - L)/2)
-        if arr[mid] == key:            
-            return mid
-        elif key < arr[mid]:
-            return binary_search(arr,key,L,mid-1)
-        elif key > arr[mid]:
-            return binary_search(arr,key,mid+1,R)
-        else:
-            return -1
+
 
 T = int(input())
 for i in range(0,T):
     n = int(input())    
-    arr = list(map(int,input().split(' ')))
-    print(balancedSum(n,(sorted(arr))[::-1]))
+    arr = list(map(int,str.strip(input()).split(' ')))
+    print(balancedSum(n,arr))
+
